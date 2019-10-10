855e50e706e30d59176f7f8a6fe874ef1163190c
Alxttar <55635802+Alxttar@users.noreply.github.com>
2019-10-10 08:55:47 -0400

    Adding channels components (#31)
    Lets you create New channels by pressing a button.
    
    New to connect enpoints to main function.

--- src/Main.js
+++ src/Main.js
@@ -71,15 +71,6 @@ const UserInfoSection = () => {
     );
 };
 
-const ChannelSection = () => {
-    return(
-        <div className="main__layout--channels">
-            Channels SECTION
-            {/* ADD YOU CODE IN THIS SECTION */}
-        </div>
-    );
-};
-
 const PersonnalChatSection = () => {
     return(
         <div className="main__layout--chat">
@@ -88,3 +79,90 @@ const PersonnalChatSection = () => {
         </div>
     );
 };
+
+const ChannelSection = () => {
+
+    const [channels, setChannels] = useState(['General']);
+    const [name, setName] = useState('');
+    const [channelName, setChannelNAme] = useState('');
+    const [showOverlay, setShowOverlay] = useState(false);
+
+    const click = () => {
+        setShowOverlay(true);
+    };
+
+    const close = () => {
+        setShowOverlay(false);
+    };
+
+    const onChannelNameChange= (event) => {
+        setName(event.target.value);
+    };
+
+    const onSubmit = () => {
+        setChannels(channels.push(name));
+    };
+
+    return(
+        <div className="main__layout--channels">
+            <CreateChannelButton channelsCount={channels.length} createChannelClick={click}/>
+            <Channels channels={channels}/>
+           { showOverlay ? <AddChannelOverlay closeOverlay={close} onChange={onChannelNameChange} onSubmit={onSubmit}/> : null }
+        </div>
+    );
+};
+
+const CreateChannelButton = (props) => {
+    return (
+        <div className="longButton__header">
+            <span className="label__header label__header--title " > Channels ({props.channelsCount})</span> 
+            <span className="label__header" onClick={props.createChannelClick}><PlusSign /></span>
+        </div>
+    );
+};
+
+const Channel = (props) => {
+    return (
+        <div className="section__channel">
+            {props.name} Channel
+        </div>
+    );
+}
+
+
+const Channels = (props) => {
+const channelsAr = props.channels;
+    return (
+        <div className="section__channels">
+            { channelsAr.map((channel) => 
+                <Channel name={channel} />
+            )}
+        </div>
+    );
+}
+
+const AddChannelOverlay = (props) => {
+    return(
+        <div className="section__overlay" >
+            <div className="container__textField">
+                <input
+                type='text'
+                className='login__field'
+                placeholder='Channel Name'
+                onChange={props.onChange}>
+                </input>
+                <input 
+                type='text'
+                className='login__field'
+                placeholder='Subject (optional)'>
+                </input>
+                <div>
+                    <span className="cancel__button" onClick={props.onSubmit}> Submit </span>
+                </div>
+                <div>
+                    <span className="cancel__button" onClick={props.closeOverlay}> Cancel </span>
+                </div>
+            </div>
+        </div>
+    );
+}
