fc1f6f2adcba85f3ebfd7e0baee02ca63c7cd8a7
t98b <bazerbachi.talal@gmail.com>
2019-11-13 20:13:03 -0500

    User Story 4 #30
    finished the sending image component which is an additional feature.

--- src/components/RightSection/InputField/InputField.js
+++ src/components/RightSection/InputField/InputField.js
@@ -1,44 +1,118 @@
-import React, { useState } from 'react';
-import { PaperclipIcon } from './../../shared/Icon'
-import './../../Main.css';
-import './../../../App.css';
-import firebase from 'firebase';
+import React, { useState } from "react";
+import { PaperclipIcon } from "./../../shared/Icon";
+import "./../../Main.css";
+import "./../../../App.css";
+import firebase from "firebase";
+import { faTasks } from "@fortawesome/free-solid-svg-icons";
 
-export const InputField = (props) => {
-    const [message, setMessage] = useState('');
-    const path = props.path;
-    const context = props.context;
-    const sendTo = props.sendTo;
-    console.log(sendTo);
+export const InputField = props => {
+  const [message, setMessage] = useState("");
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const onChange = (event) => {
-        setMessage(event.target.value);
+  const path = props.path;
+  const context = props.context;
+  const sendTo = props.sendTo;
+  console.log(sendTo);
+
+  const onChange = event => {
+    setMessage(event.target.value);
+  };
+
+  const isDirectMessage = () => {
+    return;
+  };
+  const messageType = message => {
+    const newMessage = {
+      message: message,
+      user: props.user,
+      timestamp: new Date()
+    };
+    return newMessage;
+  };
+
+  const uploadMessage = newMesssage => {
+    let messagesRef = firebase
+      .firestore()
+      .collection("channels")
+      .doc(sendTo);
+    messagesRef.update({
+      messages: firebase.firestore.FieldValue.arrayUnion(newMesssage)
+    });
+  };
+
+  const sendMessage = event => {
+    console.log(image);
+    event.preventDefault();
+    let mes = "";
+    if (message !== "") {
+      mes = messageType(message);
+      uploadMessage(mes);
     }
+    setMessage("");
+  };
 
-    const isDirectMessage = () => {
-        return 
-    }
+  const test = abc => {
+    setLoading(abc);
+  };
 
-    const sendMessage = (event) => {	
-        event.preventDefault();		
-        if (message !== '') {
-            const newMessage = {
-                message: message,
-                user: props.user,
-                timestamp: new Date()
-            }
-            let messagesRef = firebase.firestore().collection('channels').doc(sendTo);
-            messagesRef.update({
-                messages: firebase.firestore.FieldValue.arrayUnion(newMessage)
+  const test1 = abc => {
+    setImage(abc);
+  };
+
+  const imageInputHandler = () => {
+    // console.log("clicked");
+    document.querySelector("#image-input").click();
+  };
+
+  const handleImageUpload = event => {
+    var image = event.target.files[0];
+    if (image) {
+      const uploadImage = firebase
+        .storage()
+        .ref("images/" + image.name)
+        .put(image);
+
+      uploadImage.on(
+        "state_changed",
+        snapshot => {},
+        error => {
+          console.log(error);
+        },
+        () => {
+          firebase
+            .storage()
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then(url => {
+              setMessage(url);
             });
         }
-        setMessage('');
+      );
     }
+  };
 
-    return(
-        <form className="input-field-container" onSubmit={sendMessage}>
-            <span className="input-field-container--icon"><PaperclipIcon /></span>
-            <input className="input-field-container--inputField" value={message} onChange={onChange} placeholder={'Message '} />
-        </form>
-    );
+  return (
+    <form className="input-field-container" onSubmit={sendMessage}>
+      <input
+        type="file"
+        id="image-input"
+        accept="image/*"
+        onChange={handleImageUpload}
+        //onChange={handleImageUpload}
+      />
+
+      {/* <span className="input-field-container--icon" onClick={imageInputHandler}>
+        <PaperclipIcon />
+      </span> */}
+      <input
+        className="input-field-container--inputField"
+        value={message}
+        onChange={onChange}
+        placeholder={"Message "}
+      />
+    </form>
+  );
 };
